96e1b9857f8b4a5ccc41b7fe236c84271be89e1d
greatRong6 <greatrong123@163.com>
2017-04-26 16:44:36 +0800

    添加数据查询,图片浏览框架

---
+++ 代码整合/Class/VC/ImageLookVC.h
@@ -0,0 +1,13 @@
+//
+//  ImageLookVC.h
+//  代码整合
+//
+//  Created by greatRong on 2017/4/16.
+//  Copyright © 2017年 greatRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageLookVC : UIViewController
+
+@end
---
+++ 代码整合/Class/VC/ImageLookVC.m
@@ -0,0 +1,37 @@
+//
+//  ImageLookVC.m
+//  代码整合
+//
+//  Created by greatRong on 2017/4/16.
+//  Copyright © 2017年 greatRong. All rights reserved.
+//
+
+#import "ImageLookVC.h"
+
+@interface ImageLookVC ()
+
+@end
+
+@implementation ImageLookVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
